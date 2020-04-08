9a0184c84aa970c6018caf3460b260d125a60797
Sugandar <sugandar@ozzi.tech>
2020-04-08 10:38:56 +0530

    search bar added

---
+++ src/component/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import SearchBar from "./SearchBar";
+
+const App = () => {
+  return (
+    <div className="ui container" style={{ marginTop: "50px" }}>
+      <SearchBar />
+    </div>
+  );
+};
+
+export default App;
